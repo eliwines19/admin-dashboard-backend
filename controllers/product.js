67ac1325e0999961aa6683c036acaf2b0cd93ac3
Elijah Wines <wines.elijah@gmail.com>
2024-04-22 17:25:17 -0400

    created routes and controller methods for products

--- controllers/product.js
+++ controllers/product.js
@@ -1,1 +1,43 @@
 const ProductSchema = require('../models/ProductModel')
+
+exports.addProduct = async (req, res) => {
+    const { img, name, price } = req.body
+
+    const product = ProductSchema({
+        img,
+        name,
+        price
+    })
+
+    try {
+        //validations
+        if(!img || !name || !price){
+            return res.status(400).json({Error: 'All fields required'})
+        }
+        await product.save()
+        return res.status(200).json({ message: "Product Created Successfully" })
+    } catch (error) {
+        return res.status(500).json({ Error: error })
+    }
+}
+
+exports.getProducts = async (req, res) => {
+    try {
+        const products = await ProductSchema.find().sort()
+        return res.status(200).json(products)
+    } catch (error) {
+        return res.status(500).json({ Error: error })
+    }
+}
+
+exports.deleteProduct = async (req, res) => {
+    const { id } = req.params
+
+    ProductSchema.findByIdAndDelete(id)
+        .then(() => {
+            res.status(200).json({ message: "Product Deleted Successfully" })
+        })
+        .catch((error) => {
+            res.status(500).json({ Error: error })
+        })
+}
--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,13 @@
 const { addEmployee, getEmployees, deleteEmployee } = require('../controllers/employee')
+const { addProduct, getProducts, deleteProduct } = require('../controllers/product')
 
 const router = require('express').Router()
 
 router.post('/employee/new', addEmployee)
     .get('/employees', getEmployees)
     .delete('/employee/delete/:id', deleteEmployee)
+    .post('/product/new', addProduct)
+    .get('/products', getProducts)
+    .delete('/product/delete/:id', deleteProduct)
 
 module.exports = router
